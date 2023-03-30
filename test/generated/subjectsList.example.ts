4b0d0a70fe29800b37b406b8a57672f19d33194a
moh1434 <moh1434.ma@gmail.com>
2023-03-30 12:17:56 +0300

    doc(add example for the result)

---
+++ test/generated/subjectsList.example.ts
@@ -0,0 +1,11 @@
+/** this file is auto generated, don't touch it **/
+
+import { Post, Item, Subject } from '@prisma/client';
+import { JwtUser } from 'test/auth-types';
+
+export type SubjectsList = {
+  User: JwtUser;
+  Post: Post;
+  Item: Item;
+  Subject: Subject;
+};
