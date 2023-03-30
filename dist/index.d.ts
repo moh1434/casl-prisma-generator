5c60b7d1af10f128b053af01460db8a8ac3db1e5
moh1434 <moh1434.ma@gmail.com>
2023-03-30 14:58:53 +0300

    fix(index.d.ts was not include JsDocs)

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -8,5 +8,20 @@ declare const defaultPaths: {
     prismaSchemaPath: string;
     prismaClientPath: string;
 };
+/**
+ * @example
+ * const overrides: OverrideSubjects = {
+ *   User: {
+ *     typeName: 'JwtUser',
+ *     importPath: "import { JwtUser } from 'src/auth/types';",
+ *   },
+ * };
+ * generateCaslSubjectsToFile('generated/subjectsList.ts',overrides , {
+ *   prismaSchemaPath: 'prisma/schema.prisma',
+ *   prismaClientPath: '@prisma/client',
+ * });
+ * @example
+ * generateCaslSubjectsToFile('generated/subjectsList.ts');
+ */
 export declare function generateCaslSubjectsToFile(outputPath: string, overrides?: OverrideSubjects, pathsConfig?: Partial<typeof defaultPaths>): void;
 export {};
