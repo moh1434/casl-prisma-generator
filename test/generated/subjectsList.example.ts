175bbb2f5d4b2f1bf73a0313baedebc90b0a1286
moh1434 <moh1434.ma@gmail.com>
2023-04-03 12:21:42 +0300

    exclude Post model from .example.ts

--- test/generated/subjectsList.example.ts
+++ test/generated/subjectsList.example.ts
@@ -1,11 +1,10 @@
 /** this file is auto generated, don't touch it **/
 
-import { Post, Item, Subject } from '@prisma/client';
+import { Item, Subject } from '@prisma/client';
 import { JwtUser } from 'test/auth-types';
 
 export type SubjectsList = {
   User: JwtUser;
-  Post: Post;
   Item: Item;
   Subject: Subject;
 };
